8d7e45a61734d0fb0588ec1ac0bad93df7daef7f
amadi27 <65822968+amadi27@users.noreply.github.com>
2020-09-07 14:32:04 +0530

    amadi1

--- theOceanColombo/views.py
+++ theOceanColombo/views.py
@@ -18,3 +18,57 @@ auth = firebase.auth()
 
 def directHome(request):
     return render(request, "test.html")
+
+def complaintFront(request):
+    return render(request, "complaint1front.html")
+
+def complaintReply(request):
+    return render(request, "complaint2Reply.html")
+
+def complaintTyping(request):
+    return render(request, "complaint3Type.html")
+
+def complaintCheckReply(request):
+    return render(request, "complaint4CheckReply.html")
+
+def complaintUpdateReply(request):
+    return render(request, "complaint5Update.html")
+
+def complaintView(request):
+    return render(request, "complaint6View.html")
+
+def addExpenses(request):
+    return render(request, "AddExpense1.html")
+
+def Expenseslist(request):
+    return render(request, "ExpensesList.html")
+
+def AddRevenue(request):
+    return render(request, "AddRevenue3.html")
+
+def RevenueList(request):
+    return render(request, "RevenueList4.html")
+
+def Capital(request):
+    return render(request, "CapitalAccount5.html")
+
+def update6(request):
+    return render(request, "update.html")
+
+def ledgers(request):
+    return render(request, "Ledgers7.html")
+
+def ledgersView(request):
+    return render(request, "ViewLedgers8.html")
+
+def reports9(request):
+    return render(request, "Reports.html")
+
+def reportsdisplay(request):
+    return render(request, "ReportsDisplay.html")
+
+
+
+
+
+

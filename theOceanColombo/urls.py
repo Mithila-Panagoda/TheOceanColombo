46c4d69a3dfc18446376962263a4c17a77f50467
Mithila Panagoda <mithilapanagoda@gmail.com>
2020-09-07 11:03:38 +0530

    added user login page and code for authentication

--- theOceanColombo/urls.py
+++ theOceanColombo/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from. import views
+from . import views
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^$', views.directHome),
+    url(r'customerlogin/', views.custloign),
+    url(r'^logout', views.logout, name="log")
 ]
--- theOceanColombo/views.py
+++ theOceanColombo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 import pyrebase
-
+from django.contrib import auth
 config = {
     'apiKey': "AIzaSyBew42hA7iZHy7zs47WMqIg-GSBnxP-ttM",
     'authDomain': "theoceancolombo-c128a.firebaseapp.com",
@@ -13,8 +13,29 @@ config = {
 }
 firebase = pyrebase.initialize_app(config)
 
-auth = firebase.auth()
+authe = firebase.auth()
 
 
 def directHome(request):
     return render(request, "test.html")
+
+
+def custloign(request):
+    email = request.POST.get('email')
+    pwd = request.POST.get('pwd')
+    print(email)
+    print(pwd)
+    try:
+        user = authe.sign_in_with_email_and_password(email, pwd)
+    except:
+        message = "Invalid username or password please try again"
+        return render(request, "customerlogin.html", {"msg": message})
+    print(user['idToken'])
+    session_id = user['idToken']
+    request.session['uid'] = str(session_id)
+    return render(request, "test.html")
+
+
+def logout(request):
+    auth.logout(request)
+    return request(request, "test.html")

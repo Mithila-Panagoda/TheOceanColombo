85f28ed981e7b799f990ff0fd668a65ed99b87f4
Minuli <minulilokuliyanage@gmail.com>
2020-09-08 22:07:26 +0530

    added crm and inventory interfaces

--- theOceanColombo/urls.py
+++ theOceanColombo/urls.py
@@ -14,19 +14,41 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from. import views
+from . import views
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^$', views.directHome),
-    url(r'^addsupplier/', views.directaddsupplier),
+    url(r'^loadaddsupplier/', views.directaddsupplier),
+    url(r'^addsupplier/', views.addsupplier),
     url(r'^addstock/', views.directaddstock),
     url(r'^inventory/', views.directinventory),
+    url(r'^directmessages/', views.directmessages),
+    url(r'^addmessages/', views.addmessages),
+    url(r'^sendmessages/', views.sendmessages),
     url(r'^purchaseOrders/', views.directpurchaseOrders),
     url(r'^suppliers/', views.directsuppliers),
     url(r'^createcustomergroups/', views.directcreatecustomergroups),
+    url(r'customerlogin/', views.custloign),
+    url(r'^logout', views.logout, name="log"),
+    url(r'^addDeductions/', views.addAdditionalDeductions),
+    url(r'^addEarnings/', views.addEarnings),
+    url(r'^additionsDeductions/', views.additionsDeductions),
+    url(r'^EPFOfWhoseSalaryIsNeeded', views.EPFOfWhoseSalaryIsNeeded),
+    url(r'^EPFToCalculateSalary', views.EPFToCalculateSalary),
+    url(r'^PayrollManagementHome', views.directPayrollManagementHome),
+    url(r'^PaySlip', views.dirPaySlip),
+    url(r'^SalaryDetailsOfAllEmployees', views.dirSalaryDetailsOfAllEmployees),
+    url(r'^SalaryHistoryOfEmployee', views.dirSalaryHistoryOfEmployee),
+    url(r'^UpdateAdditionsOrDeductions', views.dirUpdateAdditionsOrDeductions),
+    url(r'^continuousReport', views.dirContinuousReport),
+    url(r'^HousekeepingReport', views.dirHousekeepingReport),
+    url(r'^InsertRoomDetails', views.dirInsertRoomDetails),
+    url(r'^roomDetails', views.dirRoomDetails),
+    url(r'^roomManagementHome', views.dirRoomManagementHome),
+    url(r'^updateRoomDetails', views.dirUpdateRoomDetails)
 
 ]
 
--- theOceanColombo/views.py
+++ theOceanColombo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import pyrebase
+from django.contrib import auth
 
 config = {
     'apiKey': "AIzaSyBew42hA7iZHy7zs47WMqIg-GSBnxP-ttM",
@@ -11,9 +12,11 @@ config = {
     'appId': "1:925636680144:web:da1ab5d4f3b0d3231b01d5",
     'measurementId': "G-YH7TV23J8J"
 }
-firebase = pyrebase.initialize_app(config)
 
-auth = firebase.auth()
+
+# firebase = pyrebase.initialize_app(config)
+
+# authe = firebase.auth()
 
 
 def directHome(request):
@@ -23,19 +26,138 @@ def directHome(request):
 def directaddsupplier(request):
     return render(request, "addSupplier.html")
 
+
+def addsupplier(request):
+    firebase = pyrebase.initialize_app(config)
+    db = firebase.database()
+    supplierid = request.POST.get('SupplierId')
+    suppliername = request.POST.get('SupplierName')
+    nic = request.POST.get('nic')
+    registeredno = request.POST.get('registeredno')
+    email = request.POST.get('email')
+    address = request.POST.get('address')
+
+    data = {"Suppliername": suppliername, "nic": nic, "Registeredno": registeredno, "Email": email, "Address": address}
+    db.child("Supplier").child(supplierid).set(data)
+    return render(request, "suppliers.html")
+
+
+def directmessages(request):
+    return render(request, "Messages.html")
+
+def addmessages(request):
+    firebase = pyrebase.initialize_app(config)
+    db = firebase.database()
+    messageid = request.POST.get('messageId')
+    message = request.POST.get('message')
+
+    data={"message":message}
+    db.child("Messages").child(messageid).set(data)
+    return render(request,"Messages.html")
+
+def sendmessages(request):
+    return render(request, "SendMessages.html")
+
 def directaddstock(request):
     return render(request, "addStock.html")
+
 
 def directinventory(request):
     return render(request, "inventory.html")
 
+
 def directpurchaseOrders(request):
     return render(request, "purchaseOrders.html")
 
+
 def directsuppliers(request):
     return render(request, "suppliers.html")
+
 
 def directcreatecustomergroups(request):
     return render(request, "createCustomerGroups.html")
 
 
+def custloign(request):
+    email = request.POST.get('email')
+    pwd = request.POST.get('pwd')
+    print(email)
+    print(pwd)
+    try:
+        user = authe.sign_in_with_email_and_password(email, pwd)
+    except:
+        message = "Invalid username or password please try again"
+        return render(request, "customerlogin.html", {"msg": message})
+    print(user['idToken'])
+    session_id = user['idToken']
+    request.session['uid'] = str(session_id)
+    return render(request, "test.html")
+
+
+def logout(request):
+    auth.logout(request)
+    return request(request, "test.html")
+
+
+def addAdditionalDeductions(request):
+    return render(request, "addAdditionalDeductions.html")
+
+
+def addEarnings(request):
+    return render(request, "addAdditionalEarnings.html")
+
+
+def additionsDeductions(request):
+    return render(request, "AdditionsDeductions.html")
+
+
+def EPFOfWhoseSalaryIsNeeded(request):
+    return render(request, "EPFOfWhoseSalaryIsNeeded.html")
+
+
+def EPFToCalculateSalary(request):
+    return render(request, "EPFToCalculateSalary.html")
+
+
+def directPayrollManagementHome(request):
+    return render(request, "PayrollManagementHome.html")
+
+
+def dirPaySlip(request):
+    return render(request, "PaySlip.html")
+
+
+def dirSalaryDetailsOfAllEmployees(request):
+    return render(request, "SalaryDetailsOfAllEmployees.html")
+
+
+def dirSalaryHistoryOfEmployee(request):
+    return render(request, "SalaryHistoryOfEmployee.html")
+
+
+def dirUpdateAdditionsOrDeductions(request):
+    return render(request, "UpdateAdditionsOrDeductions.html")
+
+
+def dirContinuousReport(request):
+    return render(request, "continuousReport.html")
+
+
+def dirHousekeepingReport(request):
+    return render(request, "HousekeepingReport.html")
+
+
+def dirInsertRoomDetails(request):
+    return render(request, "InsertRoomDetails.html")
+
+
+def dirRoomDetails(request):
+    return render(request, "roomDetails.html")
+
+
+def dirRoomManagementHome(request):
+    return render(request, "RoomManagementHome.html")
+
+
+def dirUpdateRoomDetails(request):
+    return render(request, "UpdateRoomDetails.html")
